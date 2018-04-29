dfd1a696520b0df177f41e4fdba47289a5504bb7
Nyárai András <andrasnyarai@gmail.com>
2018-04-30 00:42:55 +0200

    reafact():del commented lines

--- mnemonic/src/components/DoughnutChart.vue
+++ mnemonic/src/components/DoughnutChart.vue
@@ -21,8 +21,6 @@ export default {
     return {
       options: {
         rotation: -Math.PI,
-        // cutoutPercentage: 30,
-        // circumference: Math.PI,
         legend: {
           position: 'left',
         },
@@ -39,23 +37,11 @@ export default {
       labels: this.chartLabels,
       datasets: [
         {
-          // label: 'downloads',
-          // borderColor: '#249EBF',
           pointBackgroundColor: 'white',
-          // borderWidth: 1,
           pointBorderColor: '#249EBF',
           backgroundColor: this.chartColors,
-
-          // backgroundColor: [
-          //   "#FF6384",
-          //   "#63FF84",
-          //   "#84FF63",
-          //   "#8463FF",
-          //   "#6384FF"
-          // ],
           borderColor: 'black',
           borderWidth: 2,
-
           data: this.chartData,
         },
       ],
